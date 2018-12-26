db44f4d36eaf3f8b11a35024abc3ff1789467bd3
rajitha <rajitha@zippyops.com>
2018-12-26 03:01:40 -0500

    new1

--- main.js
+++ main.js
@@ -9,4 +9,4 @@ response.writeHead(200, {'Content-Type': 'text/plain'});
 response.end('Hello World\n');
 }).listen(8081);
 // Console will print the message
-console.log('Server running at http://127.0.0.1:8081/');
+console.log('Server running at http://192.168.73.4:8081/');
