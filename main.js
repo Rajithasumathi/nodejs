a08775bcbacd575ef47d029234f90ae29c150a2c
Rajithasumathi <43129700+Rajithasumathi@users.noreply.github.com>
2019-01-01 14:43:33 +0530

    Update main.js

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ http.createServer(function (request, response) {
 // HTTP Status: 200 : OK
 // Content Type: text/plain
 response.writeHead(200, {'Content-Type': 'text/plain'});
-//Send the response body as "Hello World"
-response.end('Hello welcome \n');
+//Send the response body as "Hello world"
+response.end('Hello  \n');
 }).listen(8081);
 // Console will print the message
 console.log('Server running at http://192.168.1.24:8081/');
